8e613f2f5483e4cc10cba9eaa664d3e717f1aa86
Avinesh <avineshpandey2101@gmail.com>
2021-10-02 00:52:41 +0530

    demo added

---
+++ C++/Algorithms/Demo.cpp
@@ -0,0 +1,15 @@
+//algo working in brief
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
+
+//time complexity : O()
+//space complexity: O()
+//<new thing you will learn or points to take care of>
+//general use case
+
+//link to resources
---
+++ C++/Questions/demo.cpp
@@ -0,0 +1,17 @@
+//qus or qus link
+
+//your approaches in brief
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //implementation
+}
+
+//time complexity : O()
+//space complexity: O()
+//<new thing you will learn or points to take care of>
+//general use case
+
+//link to resources
